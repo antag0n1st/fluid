67d0d61440234c88d89c7e41f3f2a8254a5e1a41
Antagonist <Antagonist@Antagonist-PC>
2012-10-13 14:18:21 +0200

    simple change

--- application/views/category.php
+++ application/views/category.php
@@ -1,7 +1,7 @@
 <div class="container o" style="margin-top: 10px;">
     <div class="left">
         <div class="img-container">
-            <img alt="" src="<?php echo base_url().'public/uploaded/featured/'.$current_category->featured_image; ?>" />
+            <img alt="" src="<?php echo base_url() . 'public/uploaded/featured/' . $current_category->featured_image; ?>" />
         </div>
         <br />
         <h3 class="article-title"><?php echo $current_category->name; ?></h3>
@@ -10,22 +10,17 @@
         </div>
         <div>
             <?php foreach ($articles as $article) : ?>
-                
-            
-            <div>
-                <div class="thumb" style="float:left;">
-                    <a href="<?php echo base_url() . 'articles/' . $article->id . '-' . $article->slug; ?>">
-                        <img src="<?php echo base_url() . 'public/uploaded/featured/thumbnails/' . $article->featured_image; ?>" alt="" width="120px" height="60px">
-                        <span class="overlay"></span>
-                    </a>
-                </div> 
-                <h3 class="title"><a href="<?php echo base_url() . 'articles/' . $article->id . '-' . $article->slug; ?>"><?php echo $article->title; ?></a></h4>
-                <p><strong><?php // FieldHelper::date_no_time_field($article->date_published); ?></strong><?php /*echo $article->description;*/ ?></p>
-            </div>
-            <div style="clear:both;"></div>
-            
-            
-            
+                <div>
+                    <div class="thumb" style="float:left;">
+                        <a href="<?php echo base_url() . 'articles/' . $article->id . '-' . $article->slug; ?>">
+                            <img src="<?php echo base_url() . 'public/uploaded/featured/thumbnails/' . $article->featured_image; ?>" alt="" width="120px" height="60px">
+                            <span class="overlay"></span>
+                        </a>
+                    </div> 
+                    <h3 class="title"><a href="<?php echo base_url() . 'articles/' . $article->id . '-' . $article->slug; ?>"><?php echo $article->title; ?></a></h4>
+                        <p><strong><?php // FieldHelper::date_no_time_field($article->date_published);   ?></strong><?php /* echo $article->description; */ ?></p>
+                </div>
+                <div style="clear:both;"></div>
             <?php endforeach; ?>
         </div>
         <?php echo $this->pagination->create_links(); ?>
